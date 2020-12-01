a82583bca11ca8d91bdf0521d76ae05abfc86b5c
Kirill Cherkalov <kirill.cherkalov@effective-soft.com>
2020-12-02 00:47:12 +0300

    node install update

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 sudo apt-get update 
 sudo apt-get upgrade -y
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-nvm --version
-nvm install --lts
-node --version
-npm install npm@latest -g
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt-get install -y nodejs
 npm -v
 npm install pm2@latest -g
